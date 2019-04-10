add0b8b71e5fccbe6b564b6f4e13b721e925b068
Alexander <berezkin05@gmail.com>
2019-04-10 12:09:24 +0300

    alter methods to return strings, add test

--- src/com/javaTask/AppRunner.java
+++ src/com/javaTask/AppRunner.java
@@ -3,57 +3,64 @@ package com.javaTask;
 public class AppRunner {
 
 	public static void main(String[] args) {
-		doMultAndPrint(1234, 123);
+		System.out.print(doMultAndPrint(1234, 123));
 	}
 
-	public static void doMultAndPrint(Integer num1, Integer num2) {
+	public static String doMultAndPrint(Integer num1, Integer num2) {
 		Integer result = num1 * num2;
-		
-		print(num1, num2, result);
+
+		return print(num1, num2, result);
 	}
 
-	private static void print(Integer num1, Integer num2, Integer result) {
+	private static String print(Integer num1, Integer num2, Integer result) {
+		StringBuffer output = new StringBuffer();
+		
 		// finding numbers lengths
 		int spaces = result.toString().length();
 		int num1len = num1.toString().length();
 		int num2len = num2.toString().length();
-		
+
 		// printing conditions
-		System.out.print(printSpaces(spaces, num1len) + num1 + "\n");
-		System.out.print(printSpaces(spaces, num2len) + num2 + "\n");
-		System.out.print(printSpaces(spaces, (num1 >= num2 ? num1len : num2len)) 
-				+ printLine(num1 >= num2 ? num1len : num2len) + "\n");
+		output.append(printSpaces(spaces, num1len) + num1 + "\n");
+		output.append(printSpaces(spaces, num2len) + num2 + "\n");
 		
 		// printing calculations
-		for (int i = 0; i < num2len; i++) {
-			Integer number = num1 * (num2 % 10);
-			num2 /= 10;
-			
-			System.out.print(printSpaces(spaces, (number.toString().length() + i)) + number + "\n");
+		if (num2len > 1) {
+			output.append(printSpaces(spaces, (num1 >= num2 ? num1len : num2len))
+					+ printLine(num1 >= num2 ? num1len : num2len) + "\n");
+
+			for (int i = 0; i < num2len; i++) {
+				Integer number = num1 * (num2 % 10);
+				num2 /= 10;
+
+				output.append(printSpaces(spaces, (number.toString().length() + i)) + number + "\n");
+			}
 		}
-		
+
 		// printing results
-		System.out.print(printLine(spaces) + "\n");
-		System.out.print(result);
+		output.append(printLine(spaces) + "\n");
+		output.append(result);
+		
+		return output.toString();
 	}
 
 	private static String printLine(int length) {
 		String sp = "";
-		
-		for(int i = 0; i < length; i++) {
+
+		for (int i = 0; i < length; i++) {
 			sp += "_";
 		}
-		
+
 		return sp;
 	}
 
 	private static String printSpaces(int spaces, int length) {
 		String sp = "";
-		
-		for(int i = 0; i < (spaces - length); i++) {
+
+		for (int i = 0; i < (spaces - length); i++) {
 			sp += " ";
 		}
-		
+
 		return sp;
 	}
 }
